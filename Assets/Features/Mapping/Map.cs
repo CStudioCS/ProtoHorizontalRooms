f9ce8726e6a1b90a16bf8260c07429e1e2326ad2
Butter <88612869+ButterOnPancakes@users.noreply.github.com>
2026-09-23 20:31:13 +0200

    Revert "Merge branch 'main' into PlayerControllerTweaks"
    This reverts commit 4f4332ad50e32e99246c7b3c48b4526c0837f079, reversing
    changes made to d35ec6ad7e1f0621518995da5c816a0500cd751d.

--- Assets/Features/Mapping/Map.cs
+++ Assets/Features/Mapping/Map.cs
@@ -6,16 +6,6 @@ public class Map : MonoBehaviour
     public Vector2 Player2Spawn;
     public void RespawnPlayer(Player player)
     {
-        if (player.IsPlayer1)
-            player.transform.position = transform.position + (Vector3)Player1Spawn;
-        else
-            player.transform.position = transform.position + (Vector3)Player2Spawn;
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(Player1Spawn, 0.2f);
-        Gizmos.DrawWireSphere(Player2Spawn, 0.2f);
+        //à compléter
     }
 }
